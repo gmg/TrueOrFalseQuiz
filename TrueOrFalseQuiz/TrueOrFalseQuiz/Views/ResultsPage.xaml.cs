ea31e39667fcf1901befe4f420684e613879630f
Gwyn Perry <gmg@users.noreply.github.com>
2020-01-27 15:00:30 +0000

    Added ResultsPage ContentView

---
+++ TrueOrFalseQuiz/TrueOrFalseQuiz/Views/ResultsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrueOrFalseQuiz.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResultsPage : ContentPage
+    {
+        public ResultsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BackToStart(object sender, EventArgs e)
+        {
+            // back to MainPage
+        }
+    }
+}
